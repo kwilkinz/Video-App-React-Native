2b1acfbeb88b8998235a9a14cda52f13b51eb3ce
Keri <105250570+kwilkinz@users.noreply.github.com>
2025-03-18 16:53:38 -0500

    style(Tabs) modified tabBarShow Styles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,10 +5,9 @@ import {images} from '@/constants/images';
 import {icons} from '@/constants/icons';
 
 const TabIcon = ({ focused, icon, title} : any) => {
-  
   if (focused) {
     return (
-      <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 justify-center items-center rounded-full overflow-hidden">
+      <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
         <Image source={icon} tintColor='#151312' className='size-5' />
         <Text className='text-secondary text-base font-semi-bold ml-2'>{title}</Text>
       </ImageBackground>
@@ -24,7 +23,28 @@ const TabIcon = ({ focused, icon, title} : any) => {
 
 const _Layout = () => {
     return (
-      <Tabs>
+      <Tabs 
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarItemStyle: {
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center'
+          },
+          tabBarStyle: {
+            backgroundColor: '#0f0D23',
+            borderRadius: 50,
+            marginHorizontal: 10, 
+            marginBottom: 36,
+            height: 52,
+            position: 'absolute',
+            overflow: 'hidden',
+            borderWidth: 1,
+            borderColor: '0f0D23'
+          }
+        }}
+      >
         <Tabs.Screen 
           name="index"
           options={{
